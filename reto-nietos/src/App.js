3ea4bcea4987aabdb3dca737a56ada2923b4cb07
JoseCarlos <jocdiazmuic@gmail.com>
2021-12-06 09:59:51 -0500

    Components react

--- reto-nietos/src/App.js
+++ reto-nietos/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./components/Switch";
+import "./App.css";
+import Switch from "./components/Switch";
+import Container from "./components/Container";
+import Actions from "./components/Actions";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      <Container>
+        <Header>
+          <Actions>
+            <Switch />{" "}
+          </Actions>
+        </Header>
+      </Container>
     </div>
   );
 }
